5f42ce9b1f4bea0c9037d8f8fabd884426d3de75
Yeison Montoya <yeison.montoya@ingeneo.com.co>
2020-07-29 18:34:40 -0500

    Childrens

--- clase-react-platzi/src/template/App.js
+++ clase-react-platzi/src/template/App.js
@@ -3,12 +3,25 @@ import logo from '../logo.svg';
 import '../assets/styles/App.scss';
 import Header from '../Component/Header';
 import Search from '../Component/Search';
+import Categories from '../Component/Categories';
+import Carousel from  '../Component/Carousel';
+import CarouselItem from '../Component/CarouselItem';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+
+      <Categories>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
     </div>
   );
 }
