1144fd180257a87acb7d2f3f685293a5f78ab2ef
Yeison Montoya <yeison.montoya@ingeneo.com.co>
2020-07-29 17:49:17 -0500

    Search bloque

--- clase-react-platzi/src/template/App.js
+++ clase-react-platzi/src/template/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../assets/styles/App.scss';
 import Header from '../Component/Header';
+import Search from '../Component/Search';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search />
     </div>
   );
 }
