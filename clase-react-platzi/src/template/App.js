f06615ae63610392192da618e170b93458612a10
Yeison Montoya <yeison.montoya@ingeneo.com.co>
2020-08-03 12:56:34 -0500

    Inicializar datos en el front de forma dinámica, una maravilla

--- clase-react-platzi/src/template/App.js
+++ clase-react-platzi/src/template/App.js
@@ -9,7 +9,7 @@ import CarouselItem from '../Component/CarouselItem';
 import Footer from '../Component/Footer';
 
 function App() {
-  const [ videos, setVideos ] = useState([]); 
+  const [ videos, setVideos ] = useState({ mylist: [], trends: [], originals: [] }); 
 
   useEffect(() => {
     // const fetchVideos = async () => {
@@ -28,31 +28,34 @@ function App() {
       .then(data => setVideos(data));
   }, []);
 
-  console.log(videos);
+  // console.log(videos);
 
   return (
     <div className="App">
       <Header />
       <Search />
 
-      <Categories title="Mi lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.mylist.length > 0 && (
+        <Categories title="Mi lista">
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
       <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
+          {videos.trends.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
         </Carousel>
       </Categories>
 
       <Categories title="Originales de Platzi Vídeo">
         <Carousel>
-          <CarouselItem />
+          {videos.originals.map(item => 
+            <CarouselItem key={item.id} {...item} />
+          )}
         </Carousel>
       </Categories>
 
