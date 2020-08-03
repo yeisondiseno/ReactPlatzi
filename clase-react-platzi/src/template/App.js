d02984554bcd135bfedce71a0a36bd361147255b
Yeison Montoya <yeison.montoya@ingeneo.com.co>
2020-08-03 12:16:01 -0500

    Se ha hecho una función asincrona para llamar la API, se usó fetch que es más corto pero también la async

--- clase-react-platzi/src/template/App.js
+++ clase-react-platzi/src/template/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { useState, useEffect} from 'react';
+// import logo from '../logo.svg';
 import '../assets/styles/App.scss';
 import Header from '../Component/Header';
 import Search from '../Component/Search';
@@ -9,6 +9,27 @@ import CarouselItem from '../Component/CarouselItem';
 import Footer from '../Component/Footer';
 
 function App() {
+  const [ videos, setVideos ] = useState([]); 
+
+  useEffect(() => {
+    // const fetchVideos = async () => {
+    //   try {
+    //     const response = await fetch('http://localhost:3000/initialState');
+    //     const data = await response.json();
+    //     return setVideos(data);
+    //   } catch {
+    //     console.log('error');
+    //   }
+    // };
+    // fetchVideos();
+    // console.log(setVideos);
+    fetch('http://localhost:3000/initialState')
+      .then(response => response.json())
+      .then(data => setVideos(data));
+  }, []);
+
+  console.log(videos);
+
   return (
     <div className="App">
       <Header />
