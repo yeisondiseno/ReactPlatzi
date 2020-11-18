a39cd9949e3c5370c5dd857f45263f852f7ab196
Yeison Montoya <yeisondiseno@gmail.com>
2020-11-17 22:27:32 -0500

    Y.Montoya: Player done
    Signed-off-by: Yeison Montoya <yeisondiseno@gmail.com>

--- clase-react-platzi/src/template/Player.jsx
+++ clase-react-platzi/src/template/Player.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter  } from "react-router-dom";
+import { connect }  from 'react-redux';
+import { getVideoSource } from '../actions';
 import '../assets/styles/component/Player.scss';
+
+import NotFound from './NotFound';
 
 const Player = props => {
 
-    return (
+    const { id } = props.match.params;
+    const [ loading, setLoading ] = useState(true)
+    const hasPlaying = Object.keys(props.playing).length > 0;
+
+    useEffect( () => {
+        props.getVideoSource(id);
+        setLoading(false)
+    }, [] )
+
+    if (loading) return <h2>Cargando...</h2>
+
+    return hasPlaying ? (
         <div className="Player">
             <video controls autoplay >
-                <source src="" type="video/mp4" />
+                <source src={props.playing.source} type="video/mp4" />
             </video>
             <div className="Player-back">
                 <button type="button" onClick={ () => props.history.goBack() }> 
@@ -15,7 +30,18 @@ const Player = props => {
                 </button>
             </div>
         </div>
-    );
+    ) : <NotFound /> ;
 }
 
-export default withRouter(Player);
+const mapStateToProps = state => {
+    return {
+        playing: state.playing,
+    }
+}
+
+
+const mapDispatchToProps = {
+    getVideoSource,
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Player));
